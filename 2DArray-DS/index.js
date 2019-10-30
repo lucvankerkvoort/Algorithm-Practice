cd0ae4d0d22b4e5190dbc1c93ed829f5ed8dfee4
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-10-29 17:14:27 -0700

    made changes to repeatedString  and started code on 2D arrays

---
+++ 2DArray-DS/index.js
@@ -0,0 +1,20 @@
+const arr = [
+  [1, 1, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0],
+  [0, 0, 2, 4, 4, 0],
+  [0, 0, 0, 2, 0, 0],
+  [0, 0, 1, 2, 4, 0]
+];
+
+const arrayFunction = arr => {
+  let comparison = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let absoluteNumber = 0;
+
+    absoluteNumber = arr[i][i];
+  }
+};
+
+arrayFunction(arr);
